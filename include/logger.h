034b4127cd014953b6da7ffdad8b71fdcf686761
f0t1h <fatihkaraoglanoglu@gmail.com>
2019-11-23 04:04:03 -0800

    Progress

--- include/logger.h
+++ include/logger.h
@@ -8,17 +8,15 @@
 #include <cstdarg>
 #include <ctime>
 
-using std::string;
 
-class Logger;
 
 template<size_t BufferSize>
 class LogLane{
 
-    char buffer[2 * BufferSize] = {0};
+    char buffer[2 * BufferSize];
 
     bool use_time;
-    string prefix;
+    std::string prefix;
     size_t buffer_index;
     FILE *file;
 
@@ -33,7 +31,11 @@ class LogLane{
             flush();
         }
     }
-   
+
+    LogLane(FILE *file): use_time(false), prefix(""), buffer_index(0), file(file){}
+    LogLane(): LogLane(stdout) {}
+
+public:
     template<class... Args>
     inline void operator()(const char *format, Args... args){
         if (BufferSize > 0){ 
@@ -61,9 +63,31 @@ class LogLane{
         }
     }
 
-    LogLane(FILE *file): use_time(false), prefix(""), buffer_index(0), file(file){}
-    LogLane(): LogLane(stdout) {}
+    inline void operator()(const char *format){
+        if (BufferSize > 0){ 
+            if(use_time){
+                time_t tt = time(NULL);
+                char *tt_str = ctime(&tt);
+                tt_str[strlen(tt_str)-1]='\0';
+                buffer_index += sprintf(buffer + buffer_index, "%s ",tt_str);
+            }
+            buffer_index += sprintf(buffer + buffer_index, "%s", prefix.c_str());
+            buffer_index += sprintf(buffer + buffer_index, "%s", format);
 
+            dump();
+        }
+        else{
+            if(use_time){
+                time_t tt = time(NULL);
+                char *tt_str = ctime(&tt);
+                tt_str[strlen(tt_str)-1]='\0';
+                buffer_index += sprintf(buffer + buffer_index, "%s ",tt_str);
+            }
+            fprintf(file, "%s", prefix.c_str());
+            fprintf(file, "%s", format);
+            fflush(file);
+        }
+    }
     friend class Logger;
 };
 
@@ -73,16 +97,16 @@ private:
 
     static const size_t BufferSize = 5 * 1024 * 1024;
 
-
-    LogLane<BufferSize> infolane;
-    LogLane<BufferSize> errorlane;
-    LogLane<0> debuglane;
-
     Logger() {}
 public:
+
+    LogLane<BufferSize> info;
+    LogLane<BufferSize> error;
+    LogLane<0> debug;
+
     ~Logger() {
-        infolane.flush();
-        errorlane.flush();
+        info.flush();
+        error.flush();
     }
 
 
@@ -105,20 +129,19 @@ public:
     }
 
     inline Logger &set_info(const std::string &str){
-        if (info_buf_size > 0) {
-            flush(infof, info_buf_size, info_buf);
-        }
-        infof = fopen(str.c_str(), "w+");
-        if (infof == NULL) {
+        info.flush();
+        info.file = fopen(str.c_str(), "w+");
+        if (info.file == NULL) {
             fputs("Could not open the info stream\n", stderr);
             exit(-1);
         }
+
         return *this;
     }
 
     inline Logger &set_debug(const std::string &str) {
-        debugf = fopen(str.c_str(), "w+");
-        if (debugf == NULL) {
+        debug.file = fopen(str.c_str(), "w+");
+        if (debug.file == NULL) {
             fputs("Could not open the debug stream\n", stderr);
             exit(-1);
         }
@@ -126,11 +149,9 @@ public:
     }
 
     inline Logger &set_error(const std::string &str){
-        if (error_buf_size > 0) {
-            flush(errorf, error_buf_size, error_buf);
-        }
-        errorf = fopen(str.c_str(), "w+");
-        if (errorf == NULL) {
+        error.flush();
+        error.file = fopen(str.c_str(), "w+");
+        if (error.file == NULL) {
             fputs("Could not open the error stream\n", stderr);
             exit(-1);
         }
@@ -138,65 +159,14 @@ public:
     }
 
     inline Logger &set_error_prefix(const std::string &prefix){
-        error_prefix = prefix;
+        error.prefix = prefix;
         return *this;
     }
 
     inline Logger &toggle_error_time() {
-        error_time_flag  = ! error_time_flag;
+        error.use_time  = ! error.use_time;
         return *this;
     }
-
-
-
-    /**
-     *  Logger command to log the debug level information
-     * @param format
-     * @param ...
-     */
-    inline void debug(const char *format, ...) {
-#ifdef DEBUG
-        va_list args;
-        va_start(args, format);
-        fprintf(debugf, format, args);
-        fflush(debugf);
-        va_end(args);
-#endif
-    }
-
-    /**
-     * Logger command to log the info level information
-     * @param format
-     * @param ...
-     */
-    inline void info(const char *format, ...) {
-        va_list args;
-        va_start(args, format);
-        info_buf_size += vsprintf(info_buf + info_buf_size, format, args);
-        va_end(args);
-        dump(infof, info_buf_size, info_buf);
-    }
-
-    /**
-     * Logger command to log the error level information
-     * @param format
-     * @param ...
-     */
-    template<typename... Args>
-    inline void error(const char *format, Args... args) {
-
-        if (error_time_flag) {
-            time_t tt = time(NULL);
-            char *tt_str = ctime(&tt);
-            tt_str[strlen(tt_str)-1]='\0';
-            error_buf_size += sprintf(error_buf + error_buf_size, "%s ",tt_str);
-        }
-        error_buf_size += sprintf(error_buf + error_buf_size, "%s", error_prefix.c_str());
-        error_buf_size += sprintf(error_buf + error_buf_size, format, args...);
-
-        dump(errorf, error_buf_size, error_buf);
-    }
-
 };
 
 #endif
--- src/test.cc
+++ src/test.cc
@@ -5,12 +5,12 @@ int main(){
     Logger::instance().info("Info Level\n");
     Logger::instance().error("Error Level\n");
     Logger::instance().toggle_error_time().set_error_prefix("[ERROR] ").error("Error Level with Prefix\n");
-    Logger::instance().error("YenYi");
+    Logger::instance().error("YenYi\n");
     Logger::instance().set_debug("debug.log").debug("Hi Debug Level in File\n");
     Logger::instance().set_debug("debug.2.log").debug("Hi Debug Level in File Two\n");
-    Logger::instance().set_error("error.log").error("Hi Error Level in File");
-    Logger::instance().set_error("error.2.log").error("Hi Error Level in File Two");
-    Logger::instance().set_info("info.log").info("Info Level in File");
-    Logger::instance().set_info("info.2.log").info("Info Level in File Two");
+    Logger::instance().set_error("error.log").error("Hi Error Level in File\n");
+    Logger::instance().set_error("error.2.log").error("Hi Error Level in File Two\n");
+    Logger::instance().set_info("info.log").info("Info Level in File\n");
+    Logger::instance().set_info("info.2.log").info("Info Level in File Two\n");
     return 0;
 }
